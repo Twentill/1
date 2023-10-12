8c260eb66caaed4821724dfa303a12afc72b2b92
Twentill <119968051+Twentill@users.noreply.github.com>
2023-10-12 17:08:51 +0500

    Create 12.10.py

---
+++ 12.10.py
@@ -0,0 +1,12 @@
+#1 zadanie
+def invert(lst):
+    result = []
+    for num in lst:
+        result.append(-num)
+    return result 
+
+#2 zadanie
+def past(h, m, s):
+    return  (h * 3600000 + m * 60000 + s * 1000)
+
+#3 zadanie
