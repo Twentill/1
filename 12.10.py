cbf6d53d1ee1ce44a7dd5961d01421c35076600d
Twentill <119968051+Twentill@users.noreply.github.com>
2023-10-12 17:38:21 +0500

    Update 12.10.py

--- 12.10.py
+++ 12.10.py
@@ -10,3 +10,35 @@ def past(h, m, s):
     return  (h * 3600000 + m * 60000 + s * 1000)
 
 #3 zadanie
+def count_sheeps(Sheeps):
+    return len([sheep for sheep in Sheeps if sheep])
+
+#4 zadanie 
+def paperwork(n, m):
+    if n < 0 or m < 0:
+        return 0
+    else:
+        return n * m
+
+#5 zadanie
+def maps(a):
+    result = []
+    for num in a:
+        result.append(num * 2)
+    return result
+
+#6 zadanie
+def descending_order(num):
+    counter = 0
+    arr = list(str(num))
+    for i in arr:
+        arr[counter]= int(i)
+        counter += 1
+    arr.sort()
+    arr = list(reversed(arr))
+    ans = ""
+    for i in arr:
+        ans += str(i)
+    return int(ans)
+
+#7 zadanie 
